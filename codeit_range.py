6eec84995660904fe9f32ce59c50a345497adbb5
minjae <32402820+kkrlee@users.noreply.github.com>
2019-01-25 13:44:20 -0500

    third upload

--- codeit_range.py
+++ codeit_range.py
@@ -1,6 +1,10 @@
 numbers = [2, 3, 5, 7, 11, 13, 17, 19]
 for i in range(len(numbers)):
     print(i, numbers[i])
-
+\n
 for i in range(0, 11):
     print("%d^%d = %d" % (2, i, 2**i))
+\n
+for i in range(1, 10):
+    for j in range(1, 10):
+        print("%d * %d = %d" % (i, j, i * j))
