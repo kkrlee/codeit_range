c0202d8ae327f5516ec0f4ffc3f0e5af20d0f2fc
minjae <32402820+kkrlee@users.noreply.github.com>
2019-01-25 13:56:51 -0500

    forth upload

--- codeit_range.py
+++ codeit_range.py
@@ -8,3 +8,9 @@ for i in range(0, 11):
 for i in range(1, 10):
     for j in range(1, 10):
         print("%d * %d = %d" % (i, j, i * j))
+
+for a in range(1, 333):
+    for b in range(a + 1, 500):
+        c = 1000 - a - b
+        if (a ** 2 + b ** 2 == c ** 2):
+            print(a * b * c)
